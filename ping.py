2dd43dfed03f29397e4d1acae2a0d524504469d9
rajagennu <raja.genupula@yahoo.com>
2014-08-11 10:17:58 +0530

    Create ping.py
    Given by Bhavani Shankar

---
+++ ping.py
@@ -0,0 +1,12 @@
+import httplib
+
+def get_status_code(host, path="/"):
+    try:
+        connect = httplib.HTTPConnection(host)
+        connect.request("HEAD", path)
+        return connect.getresponse().status
+    except StandardError:
+        return None
+
+if __name__ == '__main__'
+    print get_status_code("google.com")
